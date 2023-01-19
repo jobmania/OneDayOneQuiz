bb591ca5bdbce4067653e6aacacc1db50c84f979
jobmania <xzxz7003@naver.com>
2023-01-19 11:41:46 +0900

    택배 ing

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # OneDayOneQuiz
  다양한 자료구조 및 알고리즘에 대해서 공부
+
+1. 최댓값과 최솟값 https://school.programmers.co.kr/learn/courses/30/lessons/12939
+2. JadenCase 문자열 만들기 https://school.programmers.co.kr/learn/courses/30/lessons/12951
+3. 최솟값만들기 https://school.programmers.co.kr/learn/courses/30/lessons/12941
+4. 올바른 괄호 https://school.programmers.co.kr/learn/courses/30/lessons/12909
+5. 숫자의 표현 https://school.programmers.co.kr/learn/courses/30/lessons/12924
+6. 피보나치 수 https://school.programmers.co.kr/learn/courses/30/lessons/12945
+7. 다음 큰 숫자 https://school.programmers.co.kr/learn/courses/30/lessons/12911
+8. 실패율 https://school.programmers.co.kr/learn/courses/30/lessons/42889
+9. 짝지어 제거하기 https://school.programmers.co.kr/learn/courses/30/lessons/12973
+10. 구명보트 https://school.programmers.co.kr/learn/courses/30/lessons/42885
+11. N개의 최소공배수 https://school.programmers.co.kr/learn/courses/30/lessons/12953
+12. 예상대진표 https://school.programmers.co.kr/learn/courses/30/lessons/12985
+13. 점프와 순간이동  https://school.programmers.co.kr/learn/courses/30/lessons/12980
+14. 멀리 뛰기 https://school.programmers.co.kr/learn/courses/30/lessons/12914
+15. 롤케익 자르기 https://school.programmers.co.kr/learn/courses/30/lessons/132265
+16. 카펫  https://school.programmers.co.kr/learn/courses/30/lessons/42842
+17. 다음 큰 숫자 https://school.programmers.co.kr/learn/courses/30/lessons/12911
+18. 이진 변환 반복하기 https://school.programmers.co.kr/learn/courses/30/lessons/70129
+19. 디펜스 게임 https://school.programmers.co.kr/learn/courses/30/lessons/142085
+20. 귤 고르기  https://school.programmers.co.kr/learn/courses/30/lessons/138476
+21. 마법의 엘리베이터 https://school.programmers.co.kr/learn/courses/30/lessons/148653
+22. 우박수열 정적분 https://school.programmers.co.kr/learn/courses/30/lessons/134239
+23. 택배배달과 수거하기 https://school.programmers.co.kr/learn/courses/30/lessons/150369
--- src/main/java/com/algorizm/quiz/quizlist/_23_deliveryandcollection/DeliveryCollection.java
+++ src/main/java/com/algorizm/quiz/quizlist/_23_deliveryandcollection/DeliveryCollection.java
@@ -8,10 +8,10 @@ public class DeliveryCollection {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int cap = 2;                    // 4   // 2
-        int n = 7;                      // 5    // 2
-        int[] del = {1, 0, 2, 0, 1, 0, 2};  // 1, 0, 3, 1, 2    // 1, 0
-        int[] pic = {0, 2, 0, 1, 0, 2, 0};  // 0, 3, 0, 4, 0    // 1, 0
+        int cap = 2;                         // 4                // 2
+        int n = 7;                           // 5                // 2
+        int[] del = {1, 0, 2, 0, 1, 0, 2};  // {1, 0, 3, 1, 2}     // {1, 0}
+        int[] pic = {0, 2, 0, 1, 0, 2, 0};  // {0, 3, 0, 4, 0}     // {1, 0}
         System.out.println("답은 " + solution.solution(cap, n, del, pic));
     }
 }
@@ -22,66 +22,51 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
 
-
-        int totalDelivery = Arrays.stream(deliveries).sum();
-        int totalPickups = Arrays.stream(pickups).sum();
-        int temp = 0;
-        int temp2 = 0;
-
-
-
-        while(true){
+        while (true) {
             //순서에 대한정보.
             List<Integer> delArr = new ArrayList<>();
             List<Integer> pickArr = new ArrayList<>();
+            int temp = 0;
+            int temp2 = 0;
 
-            for(int i = deliveries.length-1; i >=0 ; i--){
-                if(deliveries[i]!=0) delArr.add(i);
-                temp += deliveries[i];
-               if(temp > cap){
-                   deliveries[i] = temp-cap;
-                   temp = cap;
-                   break;
-               }else {
-                   deliveries[i] = 0;
-               }
+            extract(cap, n, deliveries, delArr, temp);
+            extract(cap, n, pickups, pickArr, temp2);
+
+
+            if (delArr.get(0) > pickArr.get(0)) {
+                answer += 2L * (delArr.get(0) + 1);
+            } else {
+                answer += 2L * (pickArr.get(0) + 1);
             }
 
+            int totalDelivery = Arrays.stream(deliveries).sum(); // 총 갯수
+            int totalPickups = Arrays.stream(pickups).sum();
 
-
-
-            for(int i = pickups.length-1 ; i>=0 ; i--){
-                if(pickups[i]!=0) pickArr.add(i);
-                temp2 += pickups[i];
-
-                if(temp2 > cap){
-                    pickups[i] = temp2-cap;
-                    temp2= cap;
-                    break;
-                }else{
-                    pickups[i] = 0;
-                }
-            }
-
-            if(delArr.get(0)>pickArr.get(0)){
-                answer += 2L *(delArr.get(0)+1);
-            }else {
-                answer += 2L *(pickArr.get(0)+1);
-            }
-
-
-
-
-
-
-            if(totalPickups==0 & totalDelivery==0){
+            if (totalPickups == 0 && totalDelivery == 0) {
                 break;
             }
+
         }
 
-
-
-
         return answer;
+    }
+
+    private void extract(int cap, int n, int[] deliveries, List<Integer> delArr, int temp) {
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (deliveries[i] != 0){
+                delArr.add(i);
+                temp += deliveries[i];
+            }else {
+                continue;
+            }
+
+            if (temp > cap) {
+                deliveries[i] = temp - cap;
+                break;
+            } else {
+                deliveries[i] = 0;
+            }
+        }
     }
 }
