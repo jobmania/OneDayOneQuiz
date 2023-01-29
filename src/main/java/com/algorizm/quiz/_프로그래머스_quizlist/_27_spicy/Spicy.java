c9a3fd102764d8664b76011902fbf7cdb0ced662
jobmania <xzxz7003@naver.com>
2023-01-29 20:57:13 +0900

    더맵게/  PriorityQueue를 통해 구현 , 효율성테스트 통과.

--- src/main/java/com/algorizm/quiz/_프로그래머스_quizlist/_27_spicy/Spicy.java
+++ src/main/java/com/algorizm/quiz/_프로그래머스_quizlist/_27_spicy/Spicy.java
@@ -2,29 +2,34 @@ package com.algorizm.quiz._프로그래머스_quizlist._27_spicy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Spicy {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        Solution2 solution2 = new Solution2();
         int k = 7;
         int[] arr = {1, 2, 3, 9, 10, 12};
 //        System.out.println(solution.solution(arr,k));
+        System.out.println(solution2.solution(arr,k));
 
         int[] arr2 = {1,2,3,9,10,12};
 //        System.out.println(solution.solution(arr2,k));
 
         int[] arr3 = {1,1,1,1,1,1,1};
-        System.out.println(solution.solution(arr3,k));
+//        System.out.println(solution.solution(arr3,k));
 
         int[] arr4 = {15};
-        System.out.println(solution.solution(arr4,k));
+//        System.out.println(solution.solution(arr4,k));
+
+
 
     }
 
 
 }
 
-class Solution {  // 실패 시간 초과 
+class Solution {  // 실패 시간 초과
     public int solution(int[] scoville, int K) {
         int answer = 0;
         // 최솟값 섞고 이를 재배치 하면 array경우 O(n), 이진트리경우 O(logN)
@@ -68,6 +73,31 @@ class Solution {  // 실패 시간 초과
             }
         }
 
+        return answer;
+    }
+}
+class Solution2{
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        //낮은 숫자가 우선 순위인 int 형 우선순위 큐 선언
+        PriorityQueue<Integer> priorityQueueLowest = new PriorityQueue<>();
+
+        for (int j : scoville) {
+            priorityQueueLowest.add(j);
+        }
+
+        while(priorityQueueLowest.peek()<K){
+            try{
+                Integer poll1 = priorityQueueLowest.poll();  // 첫번째값, 배열에서 제거
+                Integer poll2 = priorityQueueLowest.poll();  // 첫번째값, 배열에서 제거
+                int sum = poll1 + 2 * poll2;
+                priorityQueueLowest.add(sum);
+                answer++;
+            }catch (NullPointerException e){
+                return -1;
+            }
+        }
         return answer;
     }
 }
