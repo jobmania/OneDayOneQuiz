a315647b7321408eadcf2ffee23d57b7c0bcb965
jobmania <xzxz7003@naver.com>
2023-04-13 23:27:57 +0900

    행렬의 곱셈  : for문의 i를 고려

--- src/main/java/com/algorizm/quiz/_프로그래머스_quizlist/_40_행렬의_곱셈/Matrix.java
+++ src/main/java/com/algorizm/quiz/_프로그래머스_quizlist/_40_행렬의_곱셈/Matrix.java
@@ -18,10 +18,11 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2[0].length; j++) {
+
+        for (int i = 0; i < arr1.length; i++) { // 첫번째 행렬의 행을 증가
+            for (int j = 0; j < arr2[0].length; j++) { // 두번째 행렬의 열을 증가
                 for (int k = 0; k < arr1[i].length; k++) {
-                    answer[i][j] = arr1[i][k] + arr2[k][j];
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
 
 
