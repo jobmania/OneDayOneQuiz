ab17502ccabc86f816c0fb24a5d285be3d0156d9
Son won cheol <xzxz7003@naver.com>
2023-04-24 16:39:35 +0900

    요격시스템 : 그리디  및 sort ( 진행중)

---
+++ src/main/java/com/algorizm/quiz/_프로그래머스_quizlist/_41_요격_시스템/Missile.java
@@ -0,0 +1,29 @@
+package com.algorizm.quiz._프로그래머스_quizlist._41_요격_시스템;
+
+import java.util.Arrays;
+
+public class Missile {
+    public static void main(String[] args) {
+        int[][] array = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
+
+
+        Solution solution = new Solution();
+        int result = solution.solution(array);
+        System.out.println("result = " + result);
+
+    }
+}
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+
+        
+        /// 정렬을 먼저 하자.
+
+
+
+
+        return answer;
+    }
+}
